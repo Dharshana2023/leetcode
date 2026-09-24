97cef4d2f99eb98d3d87098992ec3455121a4bf1
Dharshana2023 <155134614+Dharshana2023@users.noreply.github.com>
2026-09-24 19:15:59 +0530

    Time: 1 ms (84.05%), Space: 43 MB (18.39%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -4,16 +4,12 @@ class Solution {
         if(pattern.length()!=word.length){
             return false;
         }
-       Map<Object, Integer> index = new HashMap<>();
-
-        for (int i = 0; i < pattern.length(); ++i) {
-            if (!Objects.equals(
-                    index.put(pattern.charAt(i), i),
-                    index.put(word[i], i))) {
+        Map<Object,Integer> index=new HashMap<>();
+        for(int i=0;i<pattern.length();++i){
+            if(!Objects.equals(index.put(pattern.charAt(i),i),index.put(word[i],i))){
                 return false;
             }
         }
-
-        return true;
+            return true;
+        }
     }
-}
